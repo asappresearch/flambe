20e74ab9cf5945a642f44626c2ff4eba8f2e1faf
Jeremy Wohlwend <jeremy@asapp.com>
2019-08-21 05:05:12 +0200

    updated init file

--- flambe/nn/__init__.py
+++ flambe/nn/__init__.py
@@ -1,13 +1,15 @@
 from flambe.nn.module import Module
 from flambe.nn.softmax import SoftmaxLayer
 from flambe.nn.mos import MixtureOfSoftmax
-from flambe.nn.embedder import Embeddings, Embedder
+from flambe.nn.embedding import Embeddings, Embedder
 from flambe.nn.mlp import MLPEncoder
 from flambe.nn.rnn import RNNEncoder, PooledRNNEncoder
 from flambe.nn.cnn import CNNEncoder
 from flambe.nn.sequential import Sequential
+from flambe.nn.transformer import Transformer, TransformerEncoder, TransformerDecoder
 
 
 __all__ = ['Module', 'Embeddings', 'Embedder', 'RNNEncoder',
            'PooledRNNEncoder', 'CNNEncoder', 'MLPEncoder',
-           'SoftmaxLayer', 'MixtureOfSoftmax', 'Sequential']
+           'SoftmaxLayer', 'MixtureOfSoftmax', 'Sequential',
+           'Transformer', 'TransformerEncoder', 'TransformerDecoder']
