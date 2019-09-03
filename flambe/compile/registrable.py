4be4e26915d1adce0f1592ad5590f37b88546ee4
nickjmatthews <nmatthews@asapp.com>
2019-09-03 18:20:46 -0400

    add factory tags back to registry for error checking

--- flambe/compile/registrable.py
+++ flambe/compile/registrable.py
@@ -174,6 +174,9 @@ class Registrable(ABC):
 
         registration_helper()
         for factory_name in class_._yaml_registered_factories:
+            # Add factory tag to registry
+            factory_full_tag = f'{full_tag}.{factory_name}'
+            class_._yaml_tags[(class_, factory_name)] = [factory_full_tag]
             # Every time we register a new tag, make sure that you can
             # use each factory with that new tag
             registration_helper(factory_name)
