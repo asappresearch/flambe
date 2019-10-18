9a6eb099df79834b57b3d73a8c25a5fc953dc5cc
iitzco-asapp <44784672+iitzco-asapp@users.noreply.github.com>
2019-10-18 17:26:07 -0300

    [OPTION 2] Expose kwargs for sru (#114)
    * Add kwargs param
    
    * Improve error handling
    
    * Add rnn tests
    
    * Remove commented code
    
    * Remove repeated test

--- flambe/nn/rnn.py
+++ flambe/nn/rnn.py
@@ -1,11 +1,14 @@
 from typing import Optional, Tuple, cast
 import warnings
+import logging
 
 import torch
 from torch import nn
 from torch import Tensor
 
 from flambe.nn.module import Module
+
+logger = logging.getLogger(__name__)
 
 
 class RNNEncoder(Module):
@@ -34,7 +37,8 @@ class RNNEncoder(Module):
                  layer_norm: bool = False,
                  highway_bias: float = 0,
                  rescale: bool = True,
-                 enforce_sorted: bool = False) -> None:
+                 enforce_sorted: bool = False,
+                 **kwargs) -> None:
         """Initializes the RNNEncoder object.
 
         Parameters
@@ -61,6 +65,9 @@ class RNNEncoder(Module):
         enforce_sorted: bool
             Whether rnn should enforce that sequences are ordered by
             length. Requires True for ONNX support. Defaults to False.
+        kwargs
+            Additional parameters to be passed to SRU when building
+            the rnn.
 
         Raises
         ------
@@ -75,6 +82,11 @@ class RNNEncoder(Module):
         self.hidden_size = hidden_size
         self.enforce_sorted = enforce_sorted
         if rnn_type in ['lstm', 'gru']:
+            if kwargs:
+                logger.warn(f"The following '{kwargs}' will be ignored " +
+                            "as they are only considered when using 'sru' as " +
+                            "'rnn_type'")
+
             rnn_fn = nn.LSTM if rnn_type == 'lstm' else nn.GRU
             self.rnn = rnn_fn(input_size=input_size,
                               hidden_size=hidden_size,
@@ -83,14 +95,18 @@ class RNNEncoder(Module):
                               bidirectional=bidirectional)
         elif rnn_type == 'sru':
             from sru import SRU
-            self.rnn = SRU(input_size,
-                           hidden_size,
-                           num_layers=n_layers,
-                           dropout=dropout,
-                           bidirectional=bidirectional,
-                           layer_norm=layer_norm,
-                           rescale=rescale,
-                           highway_bias=highway_bias)
+            try:
+                self.rnn = SRU(input_size,
+                               hidden_size,
+                               num_layers=n_layers,
+                               dropout=dropout,
+                               bidirectional=bidirectional,
+                               layer_norm=layer_norm,
+                               rescale=rescale,
+                               highway_bias=highway_bias,
+                               **kwargs)
+            except TypeError:
+                raise ValueError(f"Unkown kwargs passed to SRU: {kwargs}")
         else:
             raise ValueError(f"Unkown rnn type: {rnn_type}, use of of: gru, sru, lstm")
 
