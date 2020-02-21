99769aee117a685c9aecc8547b3c83f45ab8f791
Melanie Sclar <msclar@asapp.com>
2020-02-21 12:31:41 -0300

    Change type check to work for Tensor subclasses

--- flambe/compile/component.py
+++ flambe/compile/component.py
@@ -369,7 +369,7 @@ class PickledDataLink(Registrable):
     def __call__(self, stash: Dict[str, Any]) -> Any:
         if self.obj_value is not None:
             diff_obj_stash = self.obj_value != stash[self.obj_id]
-            is_tensor = type(diff_obj_stash) == torch.Tensor
+            is_tensor = isinstance(diff_obj_stash, torch.Tensor)
 
             # == comparison between tensors returns a tensor, not bool
             if (is_tensor and torch.any(diff_obj_stash)) or (not is_tensor and diff_obj_stash):
