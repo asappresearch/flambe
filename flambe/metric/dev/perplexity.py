28f5a75a0d27d145e3e2e10e58a7be9213aec97f
Clemens Rosenbaum <crosenbaum@asapp.com>
2020-02-24 17:21:14 -0500

    - perplexity hotfix

--- flambe/metric/dev/perplexity.py
+++ flambe/metric/dev/perplexity.py
@@ -1,3 +1,5 @@
+from typing import Dict
+import numpy as np
 import torch
 
 from flambe.metric import Metric
@@ -8,7 +10,7 @@ class Perplexity(Metric):
 
     def __init__(self):
         """Perplexity, computed as CrossEntropy"""
-        self.entropy = torch.nn.CrossEntropyLoss()
+        self.entropy = torch.nn.CrossEntropyLoss(reduction='none')
 
     def compute(self, pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         """Compute the preplexity given the input and target.
@@ -28,3 +30,42 @@ class Perplexity(Metric):
         """
         entropy = self.entropy(pred, target).mean()
         return torch.exp(entropy)
+
+    def aggregate(self, state: dict, *args, **kwargs) -> Dict:
+        """Aggregates by only storing entropy per sample
+
+        Parameters
+        ----------
+        state: dict
+            the metric state
+        args: the pred, target tuple
+
+        Returns
+        -------
+        dict
+            the state dict
+        """
+        pred, target = args
+        if not state:
+            state['historic_score'] = []
+        state['historic_score'].append(self.entropy(pred, target))
+        return state
+
+    def finalize(self, state: Dict) -> float:
+        """Finalizes the metric computation
+
+        Parameters
+        ----------
+        state: dict
+            the metric state
+
+        Returns
+        -------
+        float
+            The final score.
+        """
+        if not state:
+            # call on empty state
+            return np.NaN
+        entropy = torch.cat(state['historic_score'], dim=0)
+        return torch.exp(entropy.mean()).item()
